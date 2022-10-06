436637a1b7b7a969ad49da3a5cb98cfd0e5e27f3
BrothT3 <79831328+BrothT3@users.noreply.github.com>
2022-10-06 08:16:16 +0200

    implementation af skelettet for gameState

---
+++ GameState/States/Initialize.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipsServer
+{
+    public class Initialize : IState
+    {
+        private static Initialize instance;
+        public static Initialize Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Initialize();
+                }
+                return instance;
+            }
+
+        }
+        public void Enter()
+        {
+            
+        }
+
+        public void Execute()
+        {
+            
+        }
+
+        public void Exit()
+        {
+            
+        }
+    }
+}
